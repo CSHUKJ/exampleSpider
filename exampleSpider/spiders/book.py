9d42da438cbc023418a4ec4fad44360f51f1a4b7
FuMan <480595749@qq.com>
2018-03-29 18:28:56 +0800

    删除csv文件数据之间空格.exporters.py-->CsvItemExporter-->__init__-->io.TextIOWrapper中添加newline=''

---
+++ exampleSpider/spiders/book.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BookSpider(scrapy.Spider):
+    name = 'book'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, response):
+
+        for book in response.css('article.product_pod'):
+            name = book.xpath('./h3/a/@title').extract_first()
+            price = book.css('p.price_color::text').extract_first()
+            yield {
+                'name':name,
+                'price':price,
+            }
+        # 提取下页的链接
+        next_url = response.css('ul.pager li.next a::attr(href)').extract_first()
+        if next_url:
+            # 将新的链接加入队列中， 构造新的Request对象
+            next_url = response.urljoin(next_url)
+            yield scrapy.Request(next_url, callback=self.parse)
